2ba9170a1300377554c6b2d93047b716412ca36e
Joaquin Abarca <joaquinabarca@live.com>
2023-07-25 10:34:52 -0400

    auth0 button quickguide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { ExternalApiComponent } from './pages/external-api/external-api.componen
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from '../environments/environment';
+import { AuthButtonComponent } from './components/login/auth-button/auth-button.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { environment as env } from '../environments/environment';
     HomeContentComponent,
     LoadingComponent,
     ExternalApiComponent,
-    ErrorComponent
+    ErrorComponent,
+    AuthButtonComponent,
   ],
   imports: [
     BrowserModule,
